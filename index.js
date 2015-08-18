cbd35005d68995f4dca5ce2be8828dd0081e14c9
Denis Doan <dedoin08@yahoo.de>
2015-08-18 21:24:38 +0100

    5

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 window.onload= function(){
 
-var subway = {n:["Times Square", "34th", "28th", "23th", "Union Square", "8th"], l:["8th", "6th","Union Square", "3rd", "1st"], six:["Grand Central","33rd", "28th", "23rd", "Union Square", "Astor Place"]};
+var subway = {
+n:["Times Square", "34th", "28th", "23th", "Union Square", "8th"], 
+l:["8th", "6th","Union Square", "3rd", "1st"], 
+six:["Grand Central","33rd", "28th", "23rd", "Union Square", "Astor Place"]
+};
 
 var lnListeners = function(){
-var ln = document.getElementById("Select");
-ln.addEventListener('change', function(){alert("Do great stuff")})};
+var ln = document.getElementById("Start");
+ln.addEventListener('change', function(){console.log( subway.n.indexOf(subway.n.value))}) };
 
 
 lnListeners();
