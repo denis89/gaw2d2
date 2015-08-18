a98f8f3006e4e14d2612164f6b70a278d150fb77
Denis Doan <dedoin08@yahoo.de>
2015-08-18 21:01:43 +0100

    4

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 window.onload= function(){
 
+var subway = {n:["Times Square", "34th", "28th", "23th", "Union Square", "8th"], l:["8th", "6th","Union Square", "3rd", "1st"], six:["Grand Central","33rd", "28th", "23rd", "Union Square", "Astor Place"]};
+
 var lnListeners = function(){
 var ln = document.getElementById("Select");
 ln.addEventListener('change', function(){alert("Do great stuff")})};
