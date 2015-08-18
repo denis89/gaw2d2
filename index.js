3cfbcd93a2d77d90c975130475c5ddcb43dd04d9
Denis Doan <dedoin08@yahoo.de>
2015-08-18 20:26:48 +0100

    3

--- index.js
+++ index.js
@@ -2,13 +2,11 @@ window.onload= function(){
 
 var lnListeners = function(){
 var ln = document.getElementById("Select");
+ln.addEventListener('change', function(){alert("Do great stuff")})};
 
-if(ln){
-ln.addEventListener('change', function(){alert("Do great stuff")});
-}
 
 lnListeners();
 
 
-}};
+};
 
